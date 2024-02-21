9336bb2d398ff39914deb6f3093b83af35749415
SechabaMadyibi <sechabamadyibi1@gmail.com>
2024-02-21 19:47:47 +0200

    update

--- src/Practice16_1/Main.java
+++ src/Practice16_1/Main.java
@@ -25,6 +25,8 @@ public class Main {
         
         RandomArrayAction action = new RandomArrayAction(data, 0, data.length-1, data.length/16);
         pool.invoke(action);
+
+
         
         FindMaxTask task = new FindMaxTask(data, 0, data.length-1, data.length/16);
         Integer result = pool.invoke(task);
---
+++ src/Practice17_1/Main.java
@@ -0,0 +1,14 @@
+package Practice17_1;
+
+/**
+ *
+ * @author MikeW
+ */
+public class Main {
+
+  public static void main(String[] args) {
+    
+      CalcTest.main(args);
+      
+  }
+}
